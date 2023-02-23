3645fb8da51c2c8c3ed807bcec058ed549268f23
Kevikodes <kevin@cleverprogrammer.com>
2023-02-22 20:47:21 -0800

    remove network

--- truffle-config.js
+++ truffle-config.js
@@ -1,17 +1,11 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const fs = require('fs');
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const fs = require('fs')
 module.exports = {
-  networks: {
-    inf_SushiSwap_goerli: {
-      network_id: 5,
-      gasPrice: 100000000000,
-      provider: new HDWalletProvider(fs.readFileSync('/Users/kevin/github/key.env', 'utf-8'), "https://goerli.infura.io/v3/0cad1cafc4fa4b328efbfe34d4489237")
-    }
-  },
+  networks: {},
   mocha: {},
   compilers: {
     solc: {
-      version: "0.8.16"
-    }
-  }
-};
+      version: '0.8.16',
+    },
+  },
+}
